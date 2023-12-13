90adf9c87ef9db2d70b473c6c58c3ed8aa7eda20
lvlivingston <lvlivingston@gmail.com>
2023-12-13 12:16:34 -0600

    first draft of portfolio

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Navbar from "@/components/Navbar"
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      {/* page layout that affects all urls can be placed here */}
-      <Navbar />
       <body>
         <Main />
         <NextScript />
       </body>
-      {/* i.e. footer would go here */}
-      <footer>
-        @ Leah Livingston licensed GPLv3
-      </footer>
     </Html>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,17 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>{`Leah's Portfolio`}</h1>
-     
+      <h1>{`Leah Livingston's Portfolio`}</h1>
+      <div>
+        <div>
+            I'm passionate about people and improving the daily lives of others. A Full Stack Software Engineer, driven by a curiosity for learning, the opportunity to innovate, and a fervent desire to streamline inefficiencies. Past roles include project management, customer success, sales, and marketing, which opened the door to design, architecture, and organization. The mission is to clear obstacles and improve resource distribution to create premium user experiences. Reach out!
+        </div>
+        <ul>
+            <li>Languages / Frameworks : HTML, CSS, JavaScript, Canvas, JSON, EJS, Typescript, React, SQL, Python, Mongo, DTL, Bootstrap, Express.js, React (class based and hooks), Django, Jquery, RESTful Routing, JSON api, German</li>
+            <li>Utilities : Visual Studio Code, CLI, Git, GitHub, MongoDB, Mongoose, PostgreSQL, Salesforce, HubSpot, Microsoft Office, GoToMeeting, Zoom, CompTIA Security+ Certification</li>
+            <li>Interests : Cybersecurity, Identity and Access Management, Data, & Artificial Intelligence (AI)</li>
+        </ul>
+      </div>
     </>
   )
 }
