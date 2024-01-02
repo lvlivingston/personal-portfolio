5dd1b101bc409b701d38342fa9f67a493e738b59
lvlivingston <lvlivingston@gmail.com>
2024-01-02 16:21:21 -0600

    changed to gif

--- pages/index.js
+++ pages/index.js
@@ -17,13 +17,8 @@ export default function Home() {
       <h1>{`Leah Livingston's Portfolio`}</h1>
       <div>
         <div>
-            I&apos;m passionate about people and improving the daily lives of others. A Full Stack Software Engineer, driven by a curiosity for learning, the opportunity to innovate, and a fervent desire to streamline inefficiencies. Past roles include project management, customer success, sales, and marketing, which opened the door to design, architecture, and organization. The mission is to clear obstacles and improve resource distribution to create premium user experiences. Reach out!
+        ![Loading](../public/loading.gif)
         </div>
-        <ul>
-            <li>Languages / Frameworks: HTML, CSS, JavaScript, Canvas, JSON, EJS, Typescript, SQL, Python, DTL, Next.js, Bootstrap, Materialize, Mongo, Express.js, React (class based and hooks), Django, Jquery, RESTful Routing, JSON api, German</li>
-            <li>Utilities: Visual Studio Code, CLI, Git, GitHub, MongoDB, Mongoose, PostgreSQL, Salesforce, HubSpot, Microsoft Office, GoToMeeting, Zoom, CompTIA Security+ Certification</li>
-            <li>Interests: Cybersecurity, Identity and Access Management, Data, & Artificial Intelligence (AI)</li>
-        </ul>
         <div>
           Check out my resume here{' '}
               <a
